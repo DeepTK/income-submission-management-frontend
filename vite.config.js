f06a1a5d28ac530e6d8ca726f505a376d8472e27
Deepak <deep101kamtekar@gmail.com>
2025-01-28 17:49:56 +0530

    vercel changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://income-submission-management-backend.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Rewrite path if needed
-      },
-    },
-  },
+  base: "/",
 });
